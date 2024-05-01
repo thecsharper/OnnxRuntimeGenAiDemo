61115e3b93736e134fe1f15c1b394b71dd50e763
thecsharper <si@thecsharper.com>
2024-05-01 06:27:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ML.OnnxRuntimeGenAI;
+
+var modelDirectory = @"D:\Develop\onyx-demo\Phi-3-mini-4k-instruct-onnx\cuda\cuda-int4-rtn-block-32";
+using var model = new Model(modelDirectory);
+using var tokenizer = new Tokenizer(model);
+
+while (true)
+{
+    Console.Write("Prompt: ");
+    var line = Console.ReadLine();
+    if (line == null) { continue; }
+
+    using var tokens = tokenizer.Encode(line);
+
+    using var generatorParams = new GeneratorParams(model);
+    generatorParams.SetSearchOption("max_length", 2048);
+    generatorParams.SetInputSequences(tokens);
+
+    using var generator = new Generator(model, generatorParams);
+
+    while (!generator.IsDone())
+    {
+        generator.ComputeLogits();
+        generator.GenerateNextToken();
+        var outputTokens = generator.GetSequence(0);
+        var newToken = outputTokens.Slice(outputTokens.Length - 1, 1);
+        var output = tokenizer.Decode(newToken);
+        Console.Write(output);
+    }
+    Console.WriteLine();
+}
